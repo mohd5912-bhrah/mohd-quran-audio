753ab655f3fc50745618e3de0c0c6439c806e63c
mohd5912-bhrah <mohd5912@gmail.com>
2026-01-04 20:20:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "quran-pwa-v1";
 const FILES_TO_CACHE = [
-  "/qur.html",
+  "/index.html",
   "/manifest.json"
 ];
 
@@ -15,3 +15,4 @@ self.addEventListener("fetch", e => {
     fetch(e.request).catch(() => caches.match(e.request))
   );
 });
+
